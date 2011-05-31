5ccf154f58975b451ad5f12e1cc8700f2332715d
ip2k <github@seanp2k.endjunk.com>
2011-05-30 21:04:31 -0400

    Implemented logging module with SQLite handler

--- bitbot.py
+++ bitbot.py
@@ -1,10 +1,14 @@
 import urllib2
 import json
+import logging
+import logging.handlers
+import sqlitehandler
 from ConfigParser import SafeConfigParser
 
 parser = SafeConfigParser()
 parser.read('bitbot.conf')
 
+# ---- INIT ----
 def getRate():
     result = json.load(urllib2.urlopen(parser.get('mtgox', 'ApiTickerUrl')))
     return result['ticker']['buy']
@@ -12,3 +16,25 @@ def getRate():
 print parser._sections['mtgox']
 print getRate()
 
+
+# ---- MAIN ----
+# Create a logging object (after configuring logging)
+logger = logging.getLogger('someLoggerNameLikeDebugOrWhatever')
+
+# optionally, we could define a logger like so:
+#myHandler = sqlitehandler.SQLiteHandler('debugLog.sqlite')
+
+# set the lowest level of event to log.  If undefined, defaults to logging.WARNING
+# valid levels: 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
+logger.setLevel(logging.DEBUG)
+
+# Add our custom logging handler to logger
+# The arg we're passing is the name of the SQLite database file to use
+logger.addHandler(sqlitehandler.SQLiteHandler('debugLog.sqlite'))
+
+# test out the different log levels
+logger.debug('Test 1')
+logger.info('Test 2')
+logger.warning('Test 3')
+logger.error('Test 4')
+logger.critical('Test 5')
---
+++ sqlitehandler.py
@@ -0,0 +1,20 @@
+import logging
+import sqlite3
+import time
+
+class SQLiteHandler(logging.Handler): # Inherit from logging.Handler
+    def __init__(self, filename):
+        global db
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Our custom argument
+        db = sqlite3.connect(filename) # might need to use self.filename
+        db.execute("CREATE TABLE IF NOT EXISTS debug(date text, loggername text, srclineno integer, func text, level text, msg text)")
+        db.commit()
+
+    def emit(self, record):
+        # record.message is the log message
+        thisdate = time.time()
+        print record.getMessage()
+        db.commit()
+        db.execute('INSERT INTO debug(date, loggername, srclineno, func, level, msg) VALUES(?,?,?,?,?,?)', (thisdate, record.name, record.lineno, record.funcName, record.levelname, record.msg))
