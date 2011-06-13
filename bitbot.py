5596bb129185f5fbecbf5bd11243fdfa6e5eedf9
ip2k <github@seanp2k.endjunk.com>
2011-06-12 21:21:04 -0400

    Fixing trunk repo

--- bitbot.py
+++ bitbot.py
@@ -1,40 +1,35 @@
 import urllib2
 import json
-import logging
-import logging.handlers
-import sqlitehandler
+#from debugging import debug
 from ConfigParser import SafeConfigParser
+import mtgox
 
-parser = SafeConfigParser()
-parser.read('bitbot.conf')
+cfg= SafeConfigParser()
+cfg.read('bitbot.conf')
 
 # ---- INIT ----
-def getRate():
-    result = json.load(urllib2.urlopen(parser.get('mtgox', 'ApiTickerUrl')))
+#@debug
+def getBuyRate():
+    result = json.load(urllib2.urlopen(cfg.get('mtgox', 'ApiTickerUrl')))
     return result['ticker']['buy']
 
-print parser._sections['mtgox']
-print getRate()
-
-
 # ---- MAIN ----
-# Create a logging object (after configuring logging)
-logger = logging.getLogger('someLoggerNameLikeDebugOrWhatever')
-
-# optionally, we could define a logger like so:
-#myHandler = sqlitehandler.SQLiteHandler('debugLog.sqlite')
-
-# set the lowest level of event to log.  If undefined, defaults to logging.WARNING
-# valid levels: 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
-logger.setLevel(logging.DEBUG)
-
-# Add our custom logging handler to logger
-# The arg we're passing is the name of the SQLite database file to use
-logger.addHandler(sqlitehandler.SQLiteHandler('debugLog.sqlite'))
 
 # test out the different log levels
-logger.debug('Test 1')
-logger.info('Test 2')
-logger.warning('Test 3')
-logger.error('Test 4')
-logger.critical('Test 5')
+#logger.debug('Test 1')
+#logger.info('Test 2')
+#logger.warning('Test 3')
+#logger.error('Test 4')
+#logger.critical('Test 5')
+
+#print parser._sections['mtgox']
+
+print getBuyRate()
+
+mtgox = mtgox.API(email=cfg.get('mtgox', 'email'), 
+	password=cfg.get('mtgox', 'password'),
+        ApiTickerUrl=cfg.get('mtgox', 'ApiTickerUrl'),
+        ApiGetFundsUrl=cfg.get('mtgox', 'ApiGetFundsUrl'),
+        ApiSellUrl=cfg.get('mtgox', 'ApiSellUrl'))
+
+print mtgox.getBuyRate()
