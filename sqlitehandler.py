e0ab118ccfdb05df3de19be211124636e278fd46
ip2k <github@seanp2k.endjunk.com>
2011-06-12 21:18:09 -0400

    Now using master branch to track experimental changes.  Don't expect anything here to work

--- sqlitehandler.py
+++ sqlitehandler.py
@@ -3,12 +3,15 @@ import sqlite3
 import time
 
 class SQLiteHandler(logging.Handler): # Inherit from logging.Handler
+"""
+SQLiteHandler class override specifically to support using the python logging module to log to sqlite
+"""
     def __init__(self, filename):
         global db
         # run the regular Handler __init__
         logging.Handler.__init__(self)
         # Our custom argument
-        db = sqlite3.connect(filename) # might need to use self.filename
+        db = sqlite3.connect(filename)
         db.execute("CREATE TABLE IF NOT EXISTS debug(date text, loggername text, srclineno integer, func text, level text, msg text)")
         db.commit()
 
